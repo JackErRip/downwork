605ab323e6cad6a5f750219ec310ea1c8827f7f9
John Price <136085120+JackErRip@users.noreply.github.com>
2024-06-29 00:57:53 +0800

    Initial commit

---
+++ src/sections/Section1.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Section1() {
+
+    return (
+        <div>
+            <div className='text-zinc-50 flex-row'>
+                <h1 className='font-extrabold text-left p-4 text-5xl'>Bring the top 1% of 
+                    <br />Upwork talent to your 
+                    <br /> business, your way
+                </h1>
+            </div>
+            <div className='text-zinc-50 p-4'>
+                <p>Fill the skill gaps on your team with talent specialists or 
+                    <br /> build your own dream team of experts with our full- 
+                    <br/> service, customizable Enterprise platform.
+                </p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Section1;
